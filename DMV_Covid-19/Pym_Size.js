644012083a590f11ed7f5c04197633919708a014
Adrian Nesta <adriannesta@gmail.com>
2020-04-29 12:31:05 -0400

    Adding 2020-04-28 data

---
+++ DMV_Covid-19/Pym_Size.js
@@ -0,0 +1,14 @@
+var pymChild = new pym.Child();
+function plot_size_load(e) {
+    // send updated height to parent
+    pymChild.sendHeight();
+}
+
+function plot_size_resize(e) {
+  // send updated height to parent
+  pymChild.sendHeight();
+}
+
+const body = document.querySelector('body');
+body.addEventListener("load", plot_size_load);
+body.addEventListener("resize", plot_size_resize);
