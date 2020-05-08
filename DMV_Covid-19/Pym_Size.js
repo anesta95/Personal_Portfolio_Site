0cddcb95fe5dfeba9cbbf7b9c125caaad6a1ba27
Adrian Nesta <adriannesta@gmail.com>
2020-05-08 18:32:38 -0400

    Adding 2020-05-07 data

--- DMV_Covid-19/Pym_Size.js
+++ DMV_Covid-19/Pym_Size.js
@@ -10,12 +10,13 @@ function plot_size_resize(e) {
 }
 
 window.setTimeout(plot_size_resize, 3000);
-
+window.setTimeout(plot_size_resize, 4000);
+window.setTimeout(plot_size_resize, 5000);
 const body = document.querySelector('body');
 body.addEventListener("load", plot_size_load);
 body.addEventListener("resize", plot_size_resize);
 
-window.onload = function() {
-  this.setTimeout(this.plot_size_resize, 6000);
-};
+// window.onload = function() {
+//   this.setTimeout(this.plot_size_resize, 6000);
+// };
 
